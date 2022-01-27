d366799c9edadb0ddac6cd9b9d3c3f4dd3c59840
Anisbeflo <abejarano0@ucol.mx>
2022-01-27 13:25:41 -0600

    Explanation of deque method from collection library:
    - rotate
    - appendleft
    - popleft
    - how to convert a value to string

--- 04 Collections/deque_finished.py
+++ 04 Collections/deque_finished.py
@@ -9,22 +9,29 @@ def main():
     d = collections.deque(string.ascii_lowercase)
 
     # deques support the len() function
+    # str parse the value to string
     print("Item count: " + str(len(d)))
 
     # deques can be iterated over
     for elem in d:
         print(elem.upper(), end=",")
+    print()
 
     # manipulate items from either end
     d.pop()
+    # this will pop the element at th beggining
     d.popleft()
     d.append(2)
+    # this will add it at the begging
     d.appendleft(1)
     print(d)
 
     # rotate the deque
     print(d)
-    d.rotate(1)
+    # rotate will move then spaces inside the deck
+    # when you rotate the value from the end will
+    # go to the begging
+    d.rotate(2)
     print(d)
 
 
