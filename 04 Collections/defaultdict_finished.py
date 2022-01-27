a5ee3221a08242bdbccdedd1b7935fc19e03fbf2
Anisbeflo <abejarano0@ucol.mx>
2022-01-27 12:48:10 -0600

    Explanation of defaultdict method from the collections library

--- 04 Collections/defaultdict_finished.py
+++ 04 Collections/defaultdict_finished.py
@@ -9,10 +9,20 @@ def main():
               'apple', 'grape', 'banana', 'banana']
 
     # use a dictionary to count each element
+    # this will create a default value for each
+    # new key, in this case with int and will start
+    # at 0
     fruitCounter = defaultdict(int)
 
     # Count the elements in the list
+    # for each element in fruits array
     for fruit in fruits:
+        # save the value of a key as the number
+        # key: apple, value: 1
+        # if it's the same name will access
+        # to that value and increment it
+        # fruitCounter[apple] += 1
+        # key: apple, value: 2
         fruitCounter[fruit] += 1
 
     # print the result
