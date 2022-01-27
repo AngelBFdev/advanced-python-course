9b5540c0025955e669fcc6828879fac34f9412ad
Anisbeflo <abejarano0@ucol.mx>
2022-01-27 11:17:09 -0600

    Fixed Path error

--- 02 BuiltIn Functions/iterators_finished.py
+++ 02 BuiltIn Functions/iterators_finished.py
@@ -1,10 +1,11 @@
 # use iterator functions like enumerate, zip, iter, next
-
+from pathlib import Path
 
 def main():
     # define a list of days in English and French
     days = ["Sun", "Mon", "Tue", "Wed", "Thu", "Fri", "Sat"]
     daysFr = ["Dim", "Lun", "Mar", "Mer", "Jeu", "Ven", "Sam"]
+    script_location = Path(__file__).absolute().parent
 
     # use iter to create an iterator over a collection
     i = iter(days)
@@ -13,7 +14,9 @@ def main():
     print(next(i))  # Tue
 
     # iterate using a function and a sentinel
-    with open("testfile.txt", "r") as fp:
+    with open(f"{script_location}/testfile.txt", "r") as fp:
+        # '' is the sentinel and says the end of the iterator.
+        # in this case is an empty string
         for line in iter(fp.readline, ''):
             print(line)
 
