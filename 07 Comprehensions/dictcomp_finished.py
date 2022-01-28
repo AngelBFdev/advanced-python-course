e7fcdc6c38dbffa727a4a4d1127e7eb5b2eb3eea
Anisbeflo <abejarano0@ucol.mx>
2022-01-28 12:07:52 -0600

    Explanation of comprehension in dictionaries
    - how to apply operations to an array and add it to a new dictionary.
    - How to join 2 dictionary in a new one

--- 07 Comprehensions/dictcomp_finished.py
+++ 07 Comprehensions/dictcomp_finished.py
@@ -6,6 +6,8 @@ def main():
     ctemps = [0, 12, 34, 100]
 
     # Use a comprehension to build a dictionary
+    # this make the result the value of the dictionary
+    # and the keys the original name
     tempDict = {t: (t * 9/5) + 32 for t in ctemps if t < 100}
     print(tempDict)
     print(tempDict[12])
@@ -13,6 +15,11 @@ def main():
     # Merge two dictionaries with a comprehension
     team1 = {"Jones": 24, "Jameson": 18, "Smith": 58, "Burns": 7}
     team2 = {"White": 12, "Macke": 88, "Perce": 4}
+    # what's happening is this key value is going to get inserted
+    # into this "team" dictionary, and it's going to get all the
+    # keys and values from team1 and team2. Then we're gonna loop
+    # over all of that and get the key and value to put into
+    # "newTeam"
     newTeam = {k: v for team in (team1, team2) for k, v in team.items()}
     print(newTeam)
 
