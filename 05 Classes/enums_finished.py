1f2b2529ca63498e2be3907f33e057afa3471c4f
Anisbeflo <abejarano0@ucol.mx>
2022-01-28 10:12:35 -0600

    Explanation of auto method from enum library

--- 05 Classes/enums_finished.py
+++ 05 Classes/enums_finished.py
@@ -3,12 +3,14 @@
 from enum import Enum, unique, auto
 
 
+# now it can't have a duplicate value
 @unique
 class Fruit(Enum):
     APPLE = 1
     BANANA = 2
     ORANGE = 3
     TOMATO = 4
+    # this will make it a 5
     PEAR = auto()
 
 
