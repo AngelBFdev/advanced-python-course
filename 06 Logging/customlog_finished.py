12430107d9c9ccefbd0ef53468bef8bde5644ea4
Anisbeflo <abejarano0@ucol.mx>
2022-01-28 11:53:42 -0600

    Explanation of format in logging.basicConfig
    Send:
    - date
    - time
    - level of log
    - Function Name
    - Number of line

--- 06 Logging/customlog_finished.py
+++ 06 Logging/customlog_finished.py
@@ -12,13 +12,24 @@ def anotherFunction():
 def main():
     # set the output file and debug level, and
     # use a custom formatting specification
+    # the last character is the type of var: s=string d=digit
+    # %(asctime)s call with the datefmt in basicConfig
+    # %(levelname)s is the level of the message of logging
+    # %(funcName)s name of the function called
+    # %(lineno)d number of line
+    # %(name)s key name
+    # %(message)s the string given in the log
     fmtStr = "%(asctime)s: %(levelname)s: %(funcName)s Line:%(lineno)d User:%(user)s %(message)s"
+    # %m=month, %d=day, %Y=year.
+    # %I=hour, %M=minutes, %S=seconds, %p=AM or PM
     dateStr = "%m/%d/%Y %I:%M:%S %p"
     logging.basicConfig(filename="output.log",
+                        filemode="w",
                         level=logging.DEBUG,
                         format=fmtStr,
                         datefmt=dateStr)
 
+    # with extra you can send a dictionary to fill missing keys
     logging.info("This is an info-level log message", extra=extData)
     logging.warning("This is a warning-level message", extra=extData)
     anotherFunction()
